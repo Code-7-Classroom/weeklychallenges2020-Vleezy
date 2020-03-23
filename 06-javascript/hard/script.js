7ddfbfed81161546a4ed54256c50ce0f2c12726d
Vladimir Romero <vladdromero@gmail.com>
2020-03-22 20:37:24 -0400

    updating code

--- 06-javascript/hard/script.js
+++ 06-javascript/hard/script.js
@@ -1,24 +1,29 @@
+//variables that will be input into functions
 var x = parseInt(prompt('Enter first number here.'))
-var y = parseInt(prompt('Enter seond number here.'))
+var y = parseInt(prompt('Enter second number here.'))
 
+//add
 function addition (x, y){
     return (x + y);
 
 }
 console.log(addition(x, y ));
 
+//subtract
 function subtraction (x, y){
     return (x - y);
 
 }
 console.log(subtraction(x, y));
 
+//multiply
 function multiplication (x, y){
     return (x * y);
 
 }
 console.log(multiplication(x, y));
 
+//divide
 function division (x, y){
     return (x / y);
 
--- 06-javascript/medium/script.js
+++ 06-javascript/medium/script.js
@@ -1,11 +1,17 @@
+//prompt 
 var _name = prompt("What's your name? ");
 
+//if/else statement targeting _name from prompt
 if ( _name == _name.toLowerCase()) {
+  //shows on webpage
    document.getElementById("test").innerHTML =( _name + ' you are whispering..');
+   //ahows on console
   console.log( _name + ' you are whispering..');
+
   } else if ( _name == _name.toUpperCase()) {
     document.getElementById("test").innerHTML =( _name + ' you are SHOUTING!');
     console.log(_name + ' you are SHOUTING!');
+
 } else {
     console.log(_name + ' you are speaking normally');
       document.getElementById("test").innerHTML =( _name + ' you are speaking normally.');

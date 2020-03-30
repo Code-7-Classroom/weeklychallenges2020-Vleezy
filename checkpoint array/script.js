8341dcd09fa31d425b823a86e58b3c20bbfe8bd2
Vladimir Romero <vladdromero@gmail.com>
2020-03-30 11:11:14 -0400

    array

---
+++ checkpoint array/script.js
@@ -0,0 +1,6 @@
+console.log("hello")
+var drinks = [ "water", "coffee", "tea"]; 
+var foods = [ "apple", "sandwich", "chips"]; 
+var meals = ["water", "coffee", "tea","apple", "sandwich", "chips"];
+
+console.log(meals[5], meals[4],meals[3],meals[2],meals[1]);

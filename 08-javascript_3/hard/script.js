7edb1b56844630868c7331fd1ec07454ae5a9e65
Vladimir Romero <vladdromero@gmail.com>
2020-04-06 15:25:03 -0400

    update

--- 08-javascript_3/hard/script.js
+++ 08-javascript_3/hard/script.js
@@ -1,17 +1,30 @@
+// var Pii= (function(){
+//     var ssn='1234567';
+//     return{
+//         name:"John Doe",
+//         get ssn(){
+//             return _ssn;
+//         },
+//         set ssn(_x){
+//             _ssn = _x;
+//         }
+//     }
+//   })();
+  
+//   console.log(Pii.name)
+//   Pii.ssn = "SSN not accessible";
+//   console.log( Pii.ssn);
+ 
+
 var Pii= (function(){
     var ssn='1234567';
     return{
         name:"John Doe",
-        get ssn(){
-            return _ssn;
-        },
-        set ssn(_x){
-            _ssn = _x;
+        getName(name){
+            return getName(name);
         }
     }
   })();
-  
-  console.log(Pii.name)
+  console.log(Pii.name);
   Pii.ssn = "SSN not accessible";
-  console.log( Pii.ssn);
- 
+  console.log(Pii.ssn);

687d21575d79a527d23fd67c870b215efa5bd791
Vladimir Romero <vladdromero@gmail.com>
2020-04-14 18:30:02 -0400

    update

---
+++ 10-javascript_5/easy/script.js
@@ -0,0 +1,25 @@
+
+//for loop
+const numbers = [11,2,8,49,25,6]; //global variable
+
+const largestNumber = (values) => {
+    let highest = 0; //local variable
+    for (let i=0; i<values.length; i+=1) {
+        if (values[i] > highest){
+            highest = values[i];
+        }
+    }
+    return highest;
+}
+
+console.log(largestNumber(numbers));
+
+
+/*
+//Simpler 
+
+const numbers = [1,2,3];
+///ES6 spread operator "..."
+console.log(Math.max(...numbers));
+
+*/

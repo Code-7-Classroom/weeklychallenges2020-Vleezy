97b54cf428654660c57f17d9a4b9d8109b94d4ea
Vladimir Romero <vladdromero@gmail.com>
2020-04-14 20:56:23 -0400

    update

--- 10-javascript_5/medium/script.js
+++ 10-javascript_5/medium/script.js
@@ -3,12 +3,14 @@
 const array = [1,2,3,4,5,6,7,8,9];
 
 const sum = (arr) => {
-let x = 0;
+let vlad = 0;
 
 for(let i=0; i < arr.length; i++){
-  x = x + arr[i];
-  console.log(x);
+  if (vlad = vlad + arr[i]);
 }
+ return vlad;
+
 }
+
 
 sum(array);

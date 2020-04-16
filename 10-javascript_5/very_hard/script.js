872b551e2d9ececcf5a2c230108eeb67dfd8f806
Vladimir Romero <vladdromero@gmail.com>
2020-04-16 11:19:07 -0400

    update

---
+++ 10-javascript_5/very_hard/script.js
@@ -0,0 +1,8 @@
+let str = "testing testing one two three ok";
+
+for (let i = 0; i < str.length ; i++){
+    if(str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))){
+            console.log(str.charAt(i));
+            break;
+    }
+}

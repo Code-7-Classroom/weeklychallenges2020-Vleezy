c63a241c9546a5ceec7d0a58d4c76380dfc4dead
Vladimir Romero <vladdromero@gmail.com>
2020-04-27 13:08:37 -0400

    med

---
+++ 11-nodejs/medium/node.js
@@ -0,0 +1,9 @@
+// MEDIUM: Create a node application that will read all of the planets in the solar system from a text file and print them to the console.
+// Text file: mercury, venus, earth, mars, jupiter, saturn ,uranus, neptune, pluto (I still believe in you pluto) 
+
+const fs = require('fs');
+
+fs.readFile('./planets.txt','utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})

29e5cf308054092e0844101a06b27255cf52d22e
Vladimir Romero <vladdromero@gmail.com>
2020-04-27 11:22:16 -0400

    update

--- 11-nodejs/easy/baby-steps.js
+++ 11-nodejs/easy/baby-steps.js
@@ -1,7 +1,7 @@
 // ## BABY STEPS (Exercise 2 of 13)  
-let ans = 0;
+let result = 0;
 for (i = 2; i < process.argv.length; i++) {
   ans += Number(process.argv[i]);
 }
 
-console.log(ans);
+console.log(result);

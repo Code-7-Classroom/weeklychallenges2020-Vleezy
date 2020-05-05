532bc685f3e5ede6b285945e4fcd08a56b393c63
Vladimir Romero <vladdromero@gmail.com>
2020-05-05 09:47:24 -0400

    update

--- 12-nodejs_2/medium/node.js
+++ 12-nodejs_2/medium/node.js
@@ -12,8 +12,8 @@ app.get('/employees', (req,res) => {
     res.send(data)
 })
 app.get('/employees/:id', function (req,res){
-    const sData = data.employees.find(function(employee){
-        return parseInt(req.params.id) === employee.id
+    const sData = data.employees.find(function(employees){
+        return parseInt(req.params.id) === employees.id
     })
     if(!sData){
         res.status(404).send(`INVALID EMPLOYEE ID`)

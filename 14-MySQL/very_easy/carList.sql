3737d3e034134b569e1483d57cbee7e55e2d813c
Vladimir Romero <vladdromero@gmail.com>
2020-05-28 20:02:01 -0400

    update

--- 14-MySQL/very_easy/carList.sql
+++ 14-MySQL/very_easy/carList.sql
@@ -9,6 +9,6 @@ carModel VARCHAR(255),
 carYear INT
 );
 
-INSERT INTO cars(carMake, carModel, carYear, id) VALUES ('Honda','Accord',2010 ,1),('Honda','Civic', 2012, 2),('Honda','Genesis', 2009 ,3);
+INSERT INTO cars(carMake, carModel, carYear, id) VALUES ('Honda','Accord',2010 ,1),('Honda','Civic', 2012, 2),('Honda','CR-V', 2009 ,3);
 
 SELECT*FROM cars;
